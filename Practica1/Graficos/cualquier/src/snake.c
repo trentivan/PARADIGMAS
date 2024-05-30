af9a3ab99962e20ac9cde8e193cd0338bb5f6668
trentivan <141792967+trentivan@users.noreply.github.com>
2024-05-30 13:43:31 -0700

    modified:   Practica1/Graficos/cualquier/build/Game.exe 	modified:   Practica1/Graficos/cualquier/src/snake.c 	new file:   Practica1/README.md 	deleted:    Practica1/Snake/.vscode/settings.json 	deleted:    Practica1/Snake/snake.c

--- Practica1/Graficos/cualquier/src/snake.c
+++ Practica1/Graficos/cualquier/src/snake.c
@@ -3,7 +3,7 @@
 
 #define TAMANO_CUADRADO 40
 
-// estructuras
+// Estructuras
 typedef struct Nodo {
     Vector2 posicion;
     Vector2 velocidad;
@@ -24,67 +24,70 @@ typedef struct {
     Color color;
 } Comida;
 
-// variables globales
-static const int anchoPantalla = 800;
-static const int altoPantalla = 450;
+typedef struct {
+    int anchoPantalla;
+    int altoPantalla;
+    int contadorFrames;
+    bool juegoTerminado;
+    Comida fruta;
+    Serpiente serpiente;
+    Vector2 offset;
+} Juego;
 
-static int contadorFrames = 0;
-static bool juegoTerminado = false;
+// Funciones principales
+static void InicializarJuego(Juego* juego);         
+static void ActualizarJuego(Juego* juego);          
+static void DibujarJuego(const Juego* juego);             
+static void ActualizarDibujoFrame(Juego* juego);   
 
-static Comida fruta = { 0 };
-static Serpiente serpiente;
-static Vector2 offset = { 0 };
-
-// funciones principales
-static void InicializarJuego(void);         
-static void ActualizarJuego(void);          
-static void DibujarJuego(void);             
-static void ActualizarDibujoFrame(void);   
-
-// funciones para el manejo de la serpiente
+// Funciones para el manejo de la serpiente
 static Nodo* CrearNodo(Vector2 posicion, Vector2 velocidad);
 static void AnadirNodo(Serpiente* serpiente);
 static void LiberarSerpiente(Serpiente* serpiente);
 
-// inicio del juego
+// Inicio del juego
 int main(void)
 {
-    InitWindow(anchoPantalla, altoPantalla, "snake");
-    InicializarJuego();
+    Juego juego;
+    juego.anchoPantalla = 800;
+    juego.altoPantalla = 450;
+
+    InitWindow(juego.anchoPantalla, juego.altoPantalla, "snake");
+    InicializarJuego(&juego);
     SetTargetFPS(60);
 
-    // bucle principal del juego
-    while (!WindowShouldClose())    // detectar el boton de cierre de la ventana o la tecla ESC
+    // Bucle principal del juego
+    while (!WindowShouldClose())    // Detectar el botón de cierre de la ventana o la tecla ESC
     {
-        ActualizarDibujoFrame();
+        ActualizarDibujoFrame(&juego);
     }
 
-    // libera memoria antes de cerrar el juego
-    LiberarSerpiente(&serpiente);
+    // Libera memoria antes de cerrar el juego
+    LiberarSerpiente(&(juego.serpiente));
     CloseWindow();
 
     return 0;
 }
 
-// inicializa las variables del juego
-void InicializarJuego(void)
+// Inicializa las variables del juego
+void InicializarJuego(Juego* juego)
 {
-    contadorFrames = 0;
-    juegoTerminado = false;
+    juego->contadorFrames = 0;
+    juego->juegoTerminado = false;
 
-    offset.x = anchoPantalla % TAMANO_CUADRADO;
-    offset.y = altoPantalla % TAMANO_CUADRADO;
+    juego->offset.x = juego->anchoPantalla % TAMANO_CUADRADO;
+    juego->offset.y = juego->altoPantalla % TAMANO_CUADRADO;
 
-    // inicializa la serpiente
-    serpiente.cabeza = CrearNodo((Vector2){ offset.x / 2, offset.y / 2 }, (Vector2){ TAMANO_CUADRADO, 0 });
-    serpiente.cola = serpiente.cabeza;
-    serpiente.colorCabeza = DARKBLUE;
-    serpiente.colorCuerpo = BLUE;
+    // Inicializa la serpiente
+    juego->serpiente.cabeza = CrearNodo((Vector2){ juego->offset.x / 2, juego->offset.y / 2 }, (Vector2){ TAMANO_CUADRADO, 0 });
+    juego->serpiente.cola = juego->serpiente.cabeza;
+    juego->serpiente.colorCabeza = DARKBLUE;
+    juego->serpiente.colorCuerpo = BLUE;
 
-    // inicializa la comida
-    fruta.tamano = (Vector2){ TAMANO_CUADRADO, TAMANO_CUADRADO };
-    fruta.color = SKYBLUE;
-    fruta.activa = false;
+    // Inicializa la comida
+    juego->fruta.tamano = (Vector2){ TAMANO_CUADRADO, TAMANO_CUADRADO };
+    juego->fruta.color = SKYBLUE;
+    juego->fruta.activa = false;
 }
 
 Nodo* CrearNodo(Vector2 posicion, Vector2 velocidad)
@@ -126,33 +129,33 @@ void LiberarSerpiente(Serpiente* serpiente)
     serpiente->cola = NULL;
 }
 
-// actualiza el juego
-void ActualizarJuego(void)
+// Actualiza el juego
+void ActualizarJuego(Juego* juego)
 {
-    if (!juegoTerminado)
+    if (!(juego->juegoTerminado))
     {
-        // control del jugador
-        if (IsKeyPressed(KEY_RIGHT) && (serpiente.cabeza->velocidad.x == 0))
+        // Control del jugador
+        if (IsKeyPressed(KEY_RIGHT) && (juego->serpiente.cabeza->velocidad.x == 0))
         {
-            serpiente.cabeza->velocidad = (Vector2){ TAMANO_CUADRADO, 0 };
+            juego->serpiente.cabeza->velocidad = (Vector2){ TAMANO_CUADRADO, 0 };
         }
-        if (IsKeyPressed(KEY_LEFT) && (serpiente.cabeza->velocidad.x == 0))
+        if (IsKeyPressed(KEY_LEFT) && (juego->serpiente.cabeza->velocidad.x == 0))
         {
-            serpiente.cabeza->velocidad = (Vector2){ -TAMANO_CUADRADO, 0 };
+            juego->serpiente.cabeza->velocidad = (Vector2){ -TAMANO_CUADRADO, 0 };
         }
-        if (IsKeyPressed(KEY_UP) && (serpiente.cabeza->velocidad.y == 0))
+        if (IsKeyPressed(KEY_UP) && (juego->serpiente.cabeza->velocidad.y == 0))
         {
-            serpiente.cabeza->velocidad = (Vector2){ 0, -TAMANO_CUADRADO };
+            juego->serpiente.cabeza->velocidad = (Vector2){ 0, -TAMANO_CUADRADO };
         }
-        if (IsKeyPressed(KEY_DOWN) && (serpiente.cabeza->velocidad.y == 0))
+        if (IsKeyPressed(KEY_DOWN) && (juego->serpiente.cabeza->velocidad.y == 0))
         {
-            serpiente.cabeza->velocidad = (Vector2){ 0, TAMANO_CUADRADO };
+            juego->serpiente.cabeza->velocidad = (Vector2){ 0, TAMANO_CUADRADO };
         }
 
-        // movimiento de la serpiente
-        if ((contadorFrames % 5) == 0)
+        // Movimiento de la serpiente
+        if ((juego->contadorFrames % 5) == 0)
         {
-            Nodo* actual = serpiente.cabeza;
+            Nodo* actual = juego->serpiente.cabeza;
             Vector2 posicionAnterior = actual->posicion;
             Vector2 posicionTemp;
 
@@ -168,103 +171,108 @@ void ActualizarJuego(void)
             }
         }
 
-        // colision con los límites de la matriz
-        if ((serpiente.cabeza->posicion.x >= (anchoPantalla - offset.x)) ||
-            (serpiente.cabeza->posicion.y >= (altoPantalla - offset.y)) ||
-            (serpiente.cabeza->posicion.x < 0) || (serpiente.cabeza->posicion.y < 0))
+        // Colisión con los límites de la matriz
+        if ((juego->serpiente.cabeza->posicion.x >= (juego->anchoPantalla - juego->offset.x)) ||
+            (juego->serpiente.cabeza->posicion.y >= (juego->altoPantalla - juego->offset.y)) ||
+            (juego->serpiente.cabeza->posicion.x < 0) || (juego->serpiente.cabeza->posicion.y < 0))
         {
-            juegoTerminado = true;
+            juego->juegoTerminado = true;
         }
 
-        // colision con la cola de la serpiente
-            Nodo* actual = serpiente.cabeza->siguiente;
-            while (actual != NULL)
+        // Colisión con la cola de la serpiente
+        Nodo* actual = juego->serpiente.cabeza->siguiente;
+        while (actual != NULL)
+        {
+            if ((juego->serpiente.cabeza->posicion.x == actual->posicion.x) && (juego->serpiente.cabeza->posicion.y == actual->posicion.y))
             {
-                if ((serpiente.cabeza->posicion.x == actual->posicion.x) && (serpiente.cabeza->posicion.y == actual->posicion.y))
-                {
-                    juegoTerminado = true;
-                }
-                actual = actual->siguiente;
+                juego->juegoTerminado = true;
             }
+            actual = actual->siguiente;
+        }
         
-        // colision de la cabeza de la serpiente con la comida
-        if ((serpiente.cabeza->posicion.x < (fruta.posicion.x + fruta.tamano.x) && (serpiente.cabeza->posicion.x + TAMANO_CUADRADO) > fruta.posicion.x) &&
-            (serpiente.cabeza->posicion.y < (fruta.posicion.y + fruta.tamano.y) && (serpiente.cabeza->posicion.y + TAMANO_CUADRADO) > fruta.posicion.y))
+        // Colisión de la cabeza de la serpiente con la comida
+        if ((juego->serpiente.cabeza->posicion.x < (juego->fruta.posicion.x + juego->fruta.tamano.x) && (juego->serpiente.cabeza->posicion.x + TAMANO_CUADRADO) > juego->fruta.posicion.x) &&
+            (juego->serpiente.cabeza->posicion.y < (juego->fruta.posicion.y + juego->fruta.tamano.y) && (juego->serpiente.cabeza->posicion.y + TAMANO_CUADRADO) > juego->fruta.posicion.y))
         {
-            AnadirNodo(&serpiente);
-            fruta.activa = false;
+            AnadirNodo(&(juego->serpiente));
+            juego->fruta.activa = false;
         }
 
-        // posicion de la fruta
-        if (!fruta.activa)
+        // Posición de la fruta
+        if (!(juego->fruta.activa))
         {
-            fruta.activa = true;
-            fruta.posicion = (Vector2){ GetRandomValue(0, (anchoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + offset.x / 2, GetRandomValue(0, (altoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + offset.y / 2 };
+            juego->fruta.activa = true;
+            juego->fruta.posicion = (Vector2){ GetRandomValue(0, (juego->anchoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + juego->offset.x / 2, GetRandomValue(0, (juego->altoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + juego->offset.y / 2 };
 
-            Nodo* actual = serpiente.cabeza;
+            Nodo* actual = juego->serpiente.cabeza;
             while (actual != NULL)
             {
-                while ((fruta.posicion.x == actual->posicion.x) && (fruta.posicion.y == actual->posicion.y))
+                while ((juego->fruta.posicion.x == actual->posicion.x) && (juego->fruta.posicion.y == actual->posicion.y))
                 {
-                    fruta.posicion = (Vector2){ GetRandomValue(0, (anchoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + offset.x / 2, GetRandomValue(0, (altoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + offset.y / 2 };
-                    actual = serpiente.cabeza;
+                    juego->fruta.posicion = (Vector2){ GetRandomValue(0, (juego->anchoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + juego->offset.x / 2, GetRandomValue(0, (juego->altoPantalla / TAMANO_CUADRADO) - 1) * TAMANO_CUADRADO + juego->offset.y / 2 };
+                    actual = juego->serpiente.cabeza;
                 }
                 actual = actual->siguiente;
             }
         }
 
-        contadorFrames++;
+        juego->contadorFrames++;
     }
     else
     {
         if (IsKeyPressed(KEY_ENTER))
         {
-            LiberarSerpiente(&serpiente); // libera la memoria de la serpiente antes de reiniciar el juego
-            InicializarJuego();
-            juegoTerminado = false;
+            LiberarSerpiente(&(juego->serpiente)); // Libera la memoria de la serpiente antes de reiniciar el juego
+            InicializarJuego(juego);
+            juego->juegoTerminado = false;
         }
     }
 }
 
-// dibujar juego (un frame)
-void DibujarJuego(void)
+
+// Dibuja el juego
+void DibujarJuego(const Juego* juego)
 {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    if (!juegoTerminado)
+    if (!(juego->juegoTerminado))
     {
-        // dibuja la matriz
-        for (int i = 0; i < anchoPantalla / TAMANO_CUADRADO + 1; i++)
+        // Dibuja la matriz
+        for (int i = 0; i < juego->anchoPantalla / TAMANO_CUADRADO + 1; i++)
         {
-            DrawLineV((Vector2){ TAMANO_CUADRADO * i + offset.x / 2, offset.y / 2 }, (Vector2){ TAMANO_CUADRADO * i + offset.x / 2, altoPantalla - offset.y / 2 }, LIGHTGRAY);
+            DrawLineV((Vector2){ TAMANO_CUADRADO * i + juego->offset.x / 2, juego->offset.y / 2 }, (Vector2){ TAMANO_CUADRADO * i + juego->offset.x / 2, juego->altoPantalla - juego->offset.y / 2 }, LIGHTGRAY);
         }
 
-        for (int i = 0; i < altoPantalla / TAMANO_CUADRADO + 1; i++)
+        for (int i = 0; i < juego->altoPantalla / TAMANO_CUADRADO + 1; i++)
         {
-            DrawLineV((Vector2){ offset.x / 2, TAMANO_CUADRADO * i + offset.y / 2 }, (Vector2){ anchoPantalla - offset.x / 2, TAMANO_CUADRADO * i + offset.y / 2 }, LIGHTGRAY);
+            DrawLineV((Vector2){ juego->offset.x / 2, TAMANO_CUADRADO * i + juego->offset.y / 2 }, (Vector2){ juego->anchoPantalla - juego->offset.x / 2, TAMANO_CUADRADO * i + juego->offset.y / 2 }, LIGHTGRAY);
         }
 
-        // dibuja la serpiente
-        Nodo* actual = serpiente.cabeza;
+        // Dibuja la serpiente
+        Nodo* actual = juego->serpiente.cabeza;
         while (actual != NULL)
         {
-            DrawRectangleV(actual->posicion, (Vector2){ TAMANO_CUADRADO, TAMANO_CUADRADO }, (actual == serpiente.cabeza) ? serpiente.colorCabeza : serpiente.colorCuerpo);
+            DrawRectangleV(actual->posicion, (Vector2){ TAMANO_CUADRADO, TAMANO_CUADRADO }, (actual == juego->serpiente.cabeza) ? juego->serpiente.colorCabeza : juego->serpiente.colorCuerpo);
             actual = actual->siguiente;
         }
 
-        // dibuja la comida
-        DrawRectangleV(fruta.posicion, fruta.tamano, fruta.color);
+        // Dibuja la comida
+        DrawRectangleV(juego->fruta.posicion, juego->fruta.tamano, juego->fruta.color);
     }
-    else DrawText("PRESIONA [ENTER] PARA JUGAR DE NUEVO", GetScreenWidth() / 2 - MeasureText("PRESIONA [ENTER] PARA JUGAR DE NUEVO", 20) / 2, GetScreenHeight() / 2 - 50, 20, GRAY);
+    else 
+    {
+        DrawText("PRESIONA [ENTER] PARA JUGAR DE NUEVO", GetScreenWidth() / 2 - MeasureText("PRESIONA [ENTER] PARA JUGAR DE NUEVO", 20) / 2, GetScreenHeight() / 2 - 50, 20, GRAY);
+    }
 
     EndDrawing();
 }
 
-// actualiza y dibujar (un frame)
-void ActualizarDibujoFrame(void)
+
+// Actualiza y dibujar (un frame)
+void ActualizarDibujoFrame(Juego* juego)
 {
-    ActualizarJuego();
-    DibujarJuego();
+    ActualizarJuego(juego);
+    DibujarJuego(juego);
 }
---
+++ Practica1/README.md
@@ -0,0 +1,7 @@
+# Reporte de practica 1 Paradigmas de la programacion
+
+## Hernandez Ceseña Ivan Fernando
+
+## Juego snake en  C con Raylib
+
+### Instrucciones
